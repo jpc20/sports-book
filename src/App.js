664cb8f59de3ec224e896b23ab6bb976e4ce8705
Jack Cullen <jpcullen20@gmail.com>
2020-10-01 11:27:20 -0600

    Add game cards with odds

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GamesIndex from './games/GamesIndex.js';
 import './App.css';
 
 class App extends Component {
@@ -17,25 +19,6 @@ class App extends Component {
     const games = await resp.json()
 
     this.setState({ games })
-    console.log(games);
-  }
-
-  renderGames() {
-    const today = new Date();
-    const month = (today.getMonth() + 1) < 10 ? '0' + (today.getMonth() + 1) : (today.getMonth() + 1)
-    const day = (today.getDate() + 1) < 10 ? '0' + (today.getDate()) : (today.getDate())
-    const currentDate = today.getFullYear().toString() + month + day;
-
-    return (
-      <section>
-        {this.state.games.map((game) => {
-          let gameDay = game.Date;
-          if (gameDay && parseInt(gameDay.split('T')[0].replace(/\-/g, '')) > parseInt(currentDate)) {
-            return <p>{game.AwayTeam} vs. {game.HomeTeam} {gameDay.split('T')[0].replace(/\-/g, '')} {currentDate}</p>
-          }
-        })}
-      </section>
-    );
   }
 
   render() {
@@ -45,7 +28,7 @@ class App extends Component {
           <h1>Sports Book</h1>
           <h2>Games</h2>
         </header>
-        {this.renderGames()}
+        <GamesIndex games={this.state.games}/>
       </div>
     );
   }
