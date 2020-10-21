3b39eda3fa7c3d3bcbfa0e45308837410531197c
Jack Cullen <jpcullen20@gmail.com>
2020-10-21 14:16:02 -0600

    Add reducers and actions folders

---
+++ src/redux/reducers/gameReducer.js
@@ -0,0 +1,12 @@
+import { FETCH_GAMES } from '../actions/types'
+
+const initialState = {
+  games: []
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
